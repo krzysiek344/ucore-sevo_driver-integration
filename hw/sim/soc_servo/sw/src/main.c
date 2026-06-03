80bcb09e00983509fbd5ad4569734441d3550d7c
krzysiek34 <muskrzysiek@gmail.com>
2026-06-03 17:27:49 +0200

    update soc servo integration test

--- hw/sim/soc_servo/sw/src/main.c
+++ hw/sim/soc_servo/sw/src/main.c
@@ -1,19 +1,14 @@
-#include <soc/reg.h>
-
-#define SERVO_BASE_ADDRESS       0x50000000
-
-#define SERVO_CR_ADDRESS         (SERVO_BASE_ADDRESS + 0x000)
-#define SERVO_TARGET_POS_ADDRESS (SERVO_BASE_ADDRESS + 0x008)
-#define SERVO_SCALE_ADDRESS      (SERVO_BASE_ADDRESS + 0x010)
-
-#define SERVO_CR_ENABLE_bm       0x1
-#define SERVO_CR_GO_TO_bm        0x4
+#include <soc/servo.h>
 
 int main(void)
 {
-    reg_write(SERVO_SCALE_ADDRESS, 2);
-    reg_write(SERVO_TARGET_POS_ADDRESS, 3);
-    reg_write(SERVO_CR_ADDRESS, SERVO_CR_ENABLE_bm | SERVO_CR_GO_TO_bm);
+    // init
+    servo_set_enable();
+    servo_set_inversion();
+    servo_set_scale();
+
+    // test
+    servo_go_to(3);
 
     while(1) 
     {}
