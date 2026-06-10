aa582fa686f75815769846473e960ed2aec377a0
ognisty_34 <muskrzysiek@gmail.com>
2026-06-10 20:11:22 +0200

    version with uart

--- sw/app/src/main.c
+++ sw/app/src/main.c
@@ -1,45 +1,114 @@
 #include <stdint.h>
 
-#include <soc/gpio.h>
+//#include <soc/gpio.h>
 #include <soc/servo.h>
+#include <soc/uart.h>
 
 #define TIMER_HZ        40000000u
 #define SERVO_SCALE     (TIMER_HZ / 50u)      // 50 Hz
 
+#define RX_BUF_LEN      32
+
+static uint8_t starts_with(const char *s, const char *prefix)
+{
+    while (*prefix) {
+        if (*s != *prefix)
+            return 0;
+        ++s;
+        ++prefix;
+    }
+    return 1;
+}
+
+static uint8_t parse_u32(const char *s, uint32_t *val)
+{
+    uint32_t result = 0;
+    uint8_t has_digit = 0;
+    while (*s == ' ')
+        ++s;
+    while (*s >= '0' && *s <= '9') {
+        has_digit = 1;
+        result = result * 10u + (uint32_t)(*s - '0');
+        ++s;
+    }
+    while (*s == ' ')
+        ++s;
+    if (*s != '\0')
+        return 0;
+    if (!has_digit)
+        return 0;
+    *val = result;
+    return 1;
+}
+
+static void uart_write_u32(uint32_t val)
+{
+    char buf[11];
+    int i = 0;
+    if (val == 0) {
+        uart_write_byte('0');
+        return;
+    }
+    while (val > 0) {
+        buf[i++] = (char)('0' + (val % 10u));
+        val /= 10u;
+    }
+    while (i > 0)
+        uart_write_byte((uint8_t)buf[--i]);
+}
+
 int main(void)
 {
-        servo_set_enable(1);
-        servo_set_inversion(0);
-        servo_set_scale(SERVO_SCALE);
+    char rx_buf[RX_BUF_LEN];
 
-        uint32_t prev_gpio = gpio_get_din();
-        uint32_t curr_gpio;
+    uart_init();
+    servo_set_enable(1);
+    servo_set_inversion(0);
+    servo_set_scale(SERVO_SCALE);
 
-        uint8_t sw0_curr;
-        uint8_t sw0_prev;
-        uint8_t sw1_curr;
-        uint8_t sw1_prev;
+    uart_write("booted\n");
 
-        while(1)
-        {
-                curr_gpio = gpio_get_din();
+    while (1) {
 
-                sw0_curr = (curr_gpio & 0x01) ? 1 : 0;
-                sw1_curr = (curr_gpio & 0x02) ? 1 : 0;
+        if (uart_read(rx_buf, RX_BUF_LEN) != 0) continue;
+    
 
-                sw0_prev = (prev_gpio & 0x01) ? 1 : 0;
-                sw1_prev = (prev_gpio & 0x02) ? 1 : 0;
+        if (starts_with(rx_buf, "CALIB")){
 
-                if(sw0_curr == 1 && sw0_prev == 0){
-
-                       if(!servo_is_busy())  servo_callib(1);
-                }
-
-                if(sw1_curr == 1 && sw1_prev == 0){
-
-                        if(!servo_is_busy()) servo_go_to(20);
-                }
-
-                prev_gpio = curr_gpio;
+            if (servo_is_busy()) {
+                uart_write("ERR BUSY\n");
+                continue;
+            }
+            servo_callib(1);
         }
+        
+        else if (starts_with(rx_buf, "GOTO ")){
+
+            uint32_t target_pos;
+            if (!parse_u32(rx_buf + 5, &target_pos)) {
+                uart_write("ERR ARG\n");
+                continue;
+            }
+            if (servo_is_busy()) {
+                uart_write("ERR BUSY\n");
+                continue;
+            }
+            servo_go_to(target_pos);
+        }
+        
+        else if (starts_with(rx_buf, "POS?")){
+            uart_write("POS: ");
+            uart_write_u32(servo_get_current_pos());
+            uart_write("\n");
+        } 
+        else if (starts_with(rx_buf, "STATUS?")){
+            if (servo_is_busy())
+                uart_write("STATUS BUSY\n");
+            else
+                uart_write("STATUS IDLE\n");
+        } 
+        else{
+            uart_write("ERR CMD\n");
+        }
+    }
 }
